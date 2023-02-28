2503579a87970bdebb3005a07068491146a4e2dd
Eric Johnson <eric.johnson@revention.com>
2023-02-28 17:39:24 -0600

    Add project files.

---
+++ TheCard/Program.cs
@@ -0,0 +1,61 @@
+﻿List<Card> deck = GenerateDeck();
+foreach (var card in deck)
+{
+    Console.WriteLine($"The {card.Color} {card.Rank}");
+}
+static List<Card> GenerateDeck()
+{
+    List<Card> deck = new();
+    foreach (CardColor c in Enum.GetValues(typeof(CardColor)))
+    {
+        foreach (CardRank r in Enum.GetValues(typeof(CardRank)))
+        {
+            deck.Add(new Card(c, r));
+        }
+    }
+    return deck;
+}
+
+class Card
+{
+    public CardColor Color { get; set; }
+    public CardRank Rank { get; set; }
+
+    public Card()
+    {
+        Color = CardColor.Blue;
+        Rank = CardRank.One;
+    }
+
+    public Card(CardColor c, CardRank r)
+    {
+        Color = c;
+        Rank = r;
+    }
+}
+
+enum CardColor
+{
+    Red,
+    Green,
+    Blue,
+    Yellow
+}
+
+enum CardRank
+{
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Dollar,
+    Modulus,
+    Carrot,
+    Ampersand
+}
